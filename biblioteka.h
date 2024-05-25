034919082a1f4053289d731ca30030568ece72d0
kamzob <149818908+kamzob@users.noreply.github.com>
2024-05-25 20:23:36 +0300

    Add files via upload

---
+++ biblioteka.h
@@ -0,0 +1,64 @@
+//
+//  biblioteka.h
+//  egzaminas
+//
+//  Created by Kamilė Zobėlaitė on 2024-05-25.
+//
+
+#ifndef biblioteka_h
+#define biblioteka_h
+
+//
+//  biblioteka.h
+//  V3.0
+//
+//  Created by Kamilė Zobėlaitė on 2024-05-15.
+//
+
+
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <cctype>
+#include <random>
+#include <utility>
+#include <assert.h>
+#include <map>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::srand;
+using std::rand;
+using std::vector;
+using std::setw;
+using std::fixed;
+using std::setprecision;
+using std::string;
+using std::sort;
+using std::istringstream;
+using std::ifstream;
+using std::ofstream;
+using std::exception;
+using std::numeric_limits;
+using std::streamsize;
+using std::runtime_error;
+using std::cerr;
+using std::move;
+using std::ostream;
+using std::istream;
+using std::accumulate;
+using std::map;
+
+#endif /* biblioteka_h */
+
+
