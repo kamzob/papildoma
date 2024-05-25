cf5f55678d7d18e883ede2d602dfee3fdf8b969d
kamzob <149818908+kamzob@users.noreply.github.com>
2024-05-25 21:56:24 +0300

    Add files via upload

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -28,7 +28,10 @@ void skaityti(string pavadinimas)
                 while (iss>>zodis) {
                     
                     transform(zodis.begin(), zodis.end(), zodis.begin(), ::tolower);
-                    cout << zodis << endl;
+                    cout << zodis << " ";
+                    string rezultatas;
+                    rezultatas = pasalinti_ne_zodziu_simbolius(zodis);
+                    cout << rezultatas << endl;
                     zodziai.push_back(zodis);
 
                 }
@@ -39,6 +42,20 @@ void skaityti(string pavadinimas)
     }
     failas.close();
   
-    
-   
+}
+std::string pasalinti_ne_zodziu_simbolius(std::string ivestis) {
+    std::string rezultatas;
+
+    for (char simbolis : ivestis) {
+        if ((std::isalnum(simbolis) && !isdigit(simbolis)) || yra_lietuviska_raide(simbolis)) { // Patikriname, ar simbolis yra žodžio simbolis
+            rezultatas += simbolis;
+        }
+    }
+
+    return rezultatas;
+}
+bool yra_lietuviska_raide(char simbolis) {
+    // Lietuviškos raidės su diakritiniais ženklais
+    std::string lietuviskos_raides = "ąčęėįšųūž";
+    return lietuviskos_raides.find(simbolis) != std::string::npos;
 }
