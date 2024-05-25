5696bf0a6bcf77dbb0a527f6d5379569b0bde0d8
kamzob <149818908+kamzob@users.noreply.github.com>
2024-05-25 20:24:02 +0300

    Add files via upload

---
+++ funkcijos.h
@@ -0,0 +1,13 @@
+//
+//  funkcijos.h
+//  egzaminas
+//
+//  Created by Kamilė Zobėlaitė on 2024-05-25.
+//
+
+#ifndef funkcijos_h
+#define funkcijos_h
+#include "biblioteka.h"
+void skaityti(string pavadinimas);
+
+#endif /* funkcijos_h */
